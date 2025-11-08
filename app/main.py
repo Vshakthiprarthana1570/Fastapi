9b97809832db38f28e901cf8b6d39acc66070cac
Vshakthiprarthana1570 <67454741+Vshakthiprarthana1570@users.noreply.github.com>
2025-11-08 22:22:41 +0530

    Deployment Added

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to my Application"}
+    return {"message": "Welcome to my Application from CICD Pipeline"}
 
 
 
